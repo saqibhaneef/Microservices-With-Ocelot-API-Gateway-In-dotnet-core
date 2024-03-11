4bb82e6c13277852dc69da0830530d9b1d8f6221
saqibhaniflumensoft <saqibhanif@lumensoft.biz>
2024-03-11 12:45:34 +0500

    Add project files.

---
+++ ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Product1", "Product2", "ChillProduct3", "Product4", "Product5"
+        };
+
+        public IEnumerable<dynamic> Get()
+        {
+            return Summaries;
+        }
+    }
+}
---
+++ UserAPI/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+       {
+            "User1", "User2", "User3", "User4", "User5"
+        };
+
+        public IEnumerable<dynamic> Get()
+        {
+            return Summaries;
+        }
+    }
+}
